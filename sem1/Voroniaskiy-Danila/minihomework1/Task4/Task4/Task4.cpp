c9c7c120cb6dad2a44f1dfa54a899f08973a9b90
Воронянский Данила <xgt.steamcompany@gmail.com>
2024-09-29 15:07:39 +0300

    Изменил Task4

--- sem1/Voroniaskiy-Danila/minihomework1/Task4/Task4/Task4.cpp
+++ sem1/Voroniaskiy-Danila/minihomework1/Task4/Task4/Task4.cpp
@@ -4,13 +4,18 @@ int main()
 {
     setlocale(LC_ALL, "RU");
 
-    std::string a;
+    int num;
+    int rev_num = 0;
 
     std::cout << "Введите число: ";
-    std::cin >> a;
+    std::cin >> num;
 
-    std::reverse(a.begin(),a.end());
-    std::cout << "Новое число: ";
-    std::cout << a;
+    while (num)
+    {
+        rev_num = rev_num * 10 + (num % 10);
+        num /= 10;
+    }
 
+    std::cout << "Развёрнутое число: ";
+    std::cout << rev_num << std::endl;
 }
