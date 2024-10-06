4c0103294d45ab7b582471b9c89e9c5f5d09d4e1
Воронянский Данила <xgt.steamcompany@gmail.com>
2024-10-06 14:18:30 +0300

    Залил домашку 2

---
+++ sem1/Voroniaskiy-Danila/minihomework2/Task1.cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+
+int main()
+{
+    setlocale(LC_ALL, "RU");
+
+    int num = 0;
+    int step = 0;
+
+    std::cout << "Напишите десятичное число: ";
+    std::cin >> num;
+
+    std::cout << "Напишите в степень в которую хотите перевести (8 или 16): ";
+    std::cin >> step;
+
+    if (step == 16) {
+        std::cout << "Ваше число в 16-ой системе = ";
+        std::cout << std::hex << num;
+    }
+    else if (step == 8) {
+        std::cout << "Ваше число в 8-ой системе = ";
+        std::cout << std::oct << num;
+    }
+    else {
+        std::cout << "Нет такой степени, есть только 8 или 16";
+    }
+}
---
+++ sem1/Voroniaskiy-Danila/minihomework2/Task2.cpp
@@ -0,0 +1,41 @@
+﻿#include <iostream>
+
+using namespace std;
+
+void check_num(int& num);
+
+int main()
+{
+	setlocale(LC_ALL, "RU");
+
+	int num;
+
+	std::cout << "Введите число: ";
+	std::cin >> num;
+
+	check_num(num);
+
+	std::cout << "Число = 1\n";
+	std::cout << "Ч.Т.Д.";
+	// 2 разделить на 2 == 1
+}
+
+void check_num(int& num)
+{
+	while (num > 1)
+	{
+		if (num % 2 == 0)
+		{
+			std::cout << "Число = " << num << endl;
+			num /= 2;
+
+		}
+		else if (num % 2 != 0)
+		{
+			std::cout << "Число = " << num << endl;
+			num = (num * 3 + 1) / 2;
+
+		}
+
+	}
+}
